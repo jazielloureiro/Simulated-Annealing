b2d524a04d17b9bf95849069c54db15726b45354
jazielloureiro <jazielloureiro17@gmail.com>
2024-09-24 20:55:31 -0300

    Add n-queens objective function

--- nqueens.py
+++ nqueens.py
@@ -13,9 +13,28 @@ def set_initial_state(instance):
     for i in range(instance.queens_amount):
         instance.solution[i] = random.randrange(instance.queens_amount)
 
+def objective(instance, state):
+    line_positions = [0 for _ in range(instance.queens_amount)]
+    principal_diagonal_positions = [0 for _ in range(instance.queens_amount * 2 - 1)]
+    secondary_diagonal_positions = [0 for _ in range(instance.queens_amount * 2 - 1)]
+    conflicts = 0
+
+    for i in range(instance.queens_amount):
+        conflicts += line_positions[state[i]]
+        line_positions[state[i]] += 1
+
+        conflicts += principal_diagonal_positions[i + state[i]]
+        principal_diagonal_positions[i + state[i]] += 1
+
+        conflicts += secondary_diagonal_positions[instance.queens_amount - 1 - i + state[i]]
+        secondary_diagonal_positions[instance.queens_amount - 1 - i + state[i]] += 1
+
+    return conflicts
+
 if __name__ == '__main__':
     instance = create_nqueens_problem_instance(8)
 
     set_initial_state(instance)
 
     print(instance.__dict__)
+    print(objective(instance, instance.solution))
