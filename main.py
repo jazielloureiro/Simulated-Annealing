4ac4ef97acefb838b50ce3add3ac06fc112675bd
jazielloureiro <jazielloureiro17@gmail.com>
2024-09-24 20:09:11 -0300

    Add initial temperature calculation

--- main.py
+++ main.py
@@ -41,20 +41,34 @@ def objective(instance, state):
 
     return total_value_inserted - total_value * max(0, total_weight - instance.max_weight)
 
-def get_random_neighbour_state(instance):
-    neighbour_state = instance.solution[:]
-    item_index = random.randrange(instance.itens_amount)
+def get_random_neighbour_state(state):
+    neighbour_state = state[:]
+    item_index = random.randrange(len(neighbour_state))
 
     neighbour_state[item_index] = 1 - neighbour_state[item_index]
 
     return neighbour_state
 
+def get_initial_temperature(instance, state):
+    mean = 0
+
+    for _ in range(1000):
+        state = get_random_neighbour_state(state)
+
+        objective_value = objective(instance, state)
+
+        mean += objective_value / 1000 if objective_value > 0 else 0
+    
+    return mean * math.log(30)
+
 def simulated_annealing(instance):
-    temperature, old_temperature, iteration_state = 100, 0, instance.solution
+    temperature, old_temperature, iteration_state = 0, 0, instance.solution
+
+    temperature = get_initial_temperature(instance, instance.solution)
 
     while temperature != old_temperature:
-        for _ in range(100):
-            neighbour_state = get_random_neighbour_state(instance)
+        for _ in range(200):
+            neighbour_state = get_random_neighbour_state(iteration_state)
             objective_difference = objective(instance, iteration_state) - objective(instance, neighbour_state)
 
             if objective_difference < 0:
@@ -69,8 +83,23 @@ def simulated_annealing(instance):
         temperature *= 0.95
 
 if __name__ == '__main__':
-    instance = create_knapsack_problem_instance(100, 500, 1, 50, 1, 100)
+    instance = create_knapsack_problem_instance(50, 500, 1, 50, 1, 100)
 
     set_initial_state(instance, 0.1)
     simulated_annealing(instance)
-    print(instance.__dict__)
+    
+    itens_added = []
+    itens_not_added = []
+    total_weight = 0
+    for i in range(instance.itens_amount):
+        if instance.solution[i] == 1:
+            itens_added.append(f'Value: {instance.itens[i].value}\tWeight: {instance.itens[i].weight}')
+            total_weight += instance.itens[i].weight
+        else:
+            itens_not_added.append(f'Value: {instance.itens[i].value}\tWeight: {instance.itens[i].weight}')
+
+    print('Itens not added:')
+    print('\n'.join(itens_not_added))
+    print('Itens added:')
+    print('\n'.join(itens_added))
+    print(f'Total weight: {total_weight}')
